3850f5253839d5c8c93950af435995cd581d1374
zijian hu <zijianhu@zijiandeMacBook-Pro.local>
2021-09-06 15:27:52 +0800

    app.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,24 @@
  * @Autor: zijian.hu01
  * @Date: 2021-09-04 11:37:04
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-09-06 00:20:59
+ * @LastEditTime: 2021-09-06 14:33:28
  */
 
 import "./App.css";
-import countNumber from "./HOC/countNumber";
-import changeTheme from "./HOC/changeTheme";
-import Count from "./HOC/Count";
-// import Stepper from "./RenderProps/Stepper";
+// import countNumber from "./HOC/countNumber";
+// import changeTheme from "./HOC/changeTheme";
+// import Count from "./HOC/Count";
+import Stepper from "./RenderProps/Stepper";
 // import Count from "./Hooks/Count";
 
-// HOC
-const StepperComponent = changeTheme("white")(countNumber(0)(Count));
+// HOC链式调用
+// const StepperComponent = changeTheme("white")(countNumber(0)(Count));
 
 function App() {
   return (
     <div className="App">
-      <StepperComponent />
-      {/* <Stepper /> */}
+      {/* <StepperComponent /> */}
+      <Stepper />
       {/* <Count /> */}
     </div>
   );
