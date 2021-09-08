7db294d7af60e5296e77814eb173fb63d391b486
zijian hu <zijianhu@zijiandeMacBook-Pro.local>
2021-09-08 19:34:16 +0800

    去掉注释

--- src/Hooks/useCount.js
+++ src/Hooks/useCount.js
@@ -1,11 +1,3 @@
-/*
- * @Description:
- * @Autor: zijian.hu01
- * @Date: 2021-09-05 23:18:13
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-09-05 23:27:35
- */
-
 import { useState } from "react";
 
 const useCount = (initNumber) => {
